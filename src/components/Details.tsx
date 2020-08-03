490d45d7f24dd13c4be3ff9bb01c727cf958eb9e
Anna Magdalena Bloeckner <m.bloeckner@web.de>
2020-08-03 12:44:09 +0200

    Recommendations are displayed in Details view

--- src/components/Details.tsx
+++ src/components/Details.tsx
@@ -2,16 +2,17 @@ import React from "react";
 import "../App.css";
 
 import { MovieType } from "../redux/models/data/Movie";
+import { listenerCount } from "process";
 
 type PropsType = {
   details: MovieType;
 };
 
 const DetailsComponent = (props: PropsType) => {
-  const renderImage = () => {
-    console.log(props.details.poster_path);
-    if (props.details.poster_path) {
-      let path = "http://image.tmdb.org/t/p/w185" + props.details.poster_path;
+  const renderImage = (movie: MovieType) => {
+    console.log(movie.poster_path);
+    if (movie.poster_path) {
+      let path = "http://image.tmdb.org/t/p/w185" + movie.poster_path;
       return (
         <div>
           <img src={path} className={"App-movie-poster"} alt={"movie-poster"} />
@@ -29,14 +30,49 @@ const DetailsComponent = (props: PropsType) => {
     console.log(props.details.recommendations);
   }
 
+  //Do we have recommendations for that movie?
+  //if yes, create a card view for each recommendation
+  const getRecommendationList = () => {
+    if (
+      props.details.recommendations &&
+      props.details.recommendations.length > 0
+    ) {
+      return props.details.recommendations.map((movie, index) => (
+        <div className="App-list Card" key={"movie" + movie.id}>
+          <figure>
+            {renderImage(movie)}
+            <figcaption className="Movie-figcaption">{movie.title}</figcaption>
+          </figure>
+        </div>
+      ));
+    } else {
+      return [];
+    }
+  };
+  const recommendationList = getRecommendationList();
+
+  //Create the recommendations part of the details view.
+  //If no recommendations are available, we show nothing.
+  const recommendations = () => {
+    if (recommendationsAvailable) {
+      return (
+        <div className="RecommendationView">
+          <h4 className="Recommendations-title">Recommendations:</h4>
+          <div className="RecommendationContainer">{recommendationList}</div>
+        </div>
+      );
+    }
+  };
+
+  //Create the Details view
   return (
     <div className="App-content">
       <div className="Card DetailsCard">
         <div className="App-details-data">
-          {renderImage()}
+          {renderImage(props.details)}
           <div>
             <h3>{props.details.title}</h3>
-            <p>{props.details.overview}</p>
+            <p className="Details-description">{props.details.overview}</p>
             <p>Release: {props.details.release_date}</p>
             <p>
               Recommendations available:{" "}
@@ -44,6 +80,7 @@ const DetailsComponent = (props: PropsType) => {
             </p>
           </div>
         </div>
+        {recommendations()}
       </div>
     </div>
   );
